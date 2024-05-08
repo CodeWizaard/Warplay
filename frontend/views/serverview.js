83aad90fbda5b6b53936d1c342aa179c3dc07c03
Кузьмин Кирилл Вячеславович <143796958+CodeWizaard@users.noreply.github.com>
2024-05-08 17:49:12 +0500

    refractoring server code

--- frontend/views/serverview.js
+++ frontend/views/serverview.js
@@ -1,20 +1,27 @@
-var myDiv = document.getElementById('content');
+
+var myDiv = document.getElementById('block_server');
 
 fetch('http://localhost:3000/servers') // Отправка GET-запроса на сервер
   .then(response => response.json())
   .then(data => {
+    create_card(data);
+    console.log('Создание карточек серверов завершено');
+  })
+  .catch(error => console.error('Ошибка при получении данных:', error));
+ 
+  function create_card(data) {
       // Создаем новый div для каждого свойства объекта data
       for (let indexData in data) {
         if (data.hasOwnProperty(indexData)) {
             let newDiv = document.createElement('div');
-            newDiv.textContent = indexData + ': ' + data[indexData]; // Выводим ключ и значение свойства
+            
+     
+            newDiv.textContent = indexData + ': ' + data[indexData].location; 
+            newDiv.classList.add('card_server');// Выводим ключ и значение свойства
             myDiv.appendChild(newDiv); // Добавляем новый div в основной контейнер
         }
+        console.log('Создание карточки №'+ data[indexData]);
     }
-    console.log(data);
-  })
-  .catch(error => console.error('Ошибка при получении данных:', error));
- 
-
+}
 
 
