99c028b1bf82039b2cefc4dfda71572975313d37
Кузьмин Кирилл Вячеславович <143796958+CodeWizaard@users.noreply.github.com>
2024-05-08 18:08:59 +0500

    Update card "Add ServerName"

--- frontend/views/serverview.js
+++ frontend/views/serverview.js
@@ -9,18 +9,30 @@ fetch('http://localhost:3000/servers') // Отправка GET-запроса н
   })
   .catch(error => console.error('Ошибка при получении данных:', error));
  
-  function create_card(data) {
+function create_card(data) {
       // Создаем новый div для каждого свойства объекта data
       for (let indexData in data) {
         if (data.hasOwnProperty(indexData)) {
-            let newDiv = document.createElement('div');
-            
-     
-            newDiv.textContent = indexData + ': ' + data[indexData].location; 
-            newDiv.classList.add('card_server');// Выводим ключ и значение свойства
-            myDiv.appendChild(newDiv); // Добавляем новый div в основной контейнер
+            create_card(data);
         }
         console.log('Создание карточки №'+ data[indexData]);
+    }
+}
+function create_card(data) {
+    // Создаем новый div для каждого свойства объекта data
+    for (let indexData in data) {
+      if (data.hasOwnProperty(indexData)) {
+          let newDiv = document.createElement('div');
+          create_subsystem_card(newDiv, indexData); // Добавляем новый div в основной контейнер
+      }
+      console.log('Создание карточки №'+ data[indexData]);
+  }
+
+    function create_subsystem_card(newDiv, indexData) {
+        let divServerName = document.createElement('div');
+        divServerName.textContent = indexData + ': ' + data[indexData].serverName;
+        divServerName.classList.add('card_server'); // Выводим ключ и значение свойства
+        myDiv.appendChild(divServerName);
     }
 }
 
